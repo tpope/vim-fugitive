tpope/vim-fugitive

commit ed9e21fb9bdbda7989c1c9190ca830d36a5df4e2

Support :Git difftool --check
Resolves: https://github.com/tpope/vim-fugitive/issues/2303

autoload/fugitive.vim
@@ -5466,7 +5466,9 @@ function! s:ToolItems(state, from, to, offsets, text, ...) abort
     endif
     call add(items, item)
   endfor
-  let items[-1].context = {'diff': items[0:-2]}
+  if get(offsets, 0, 0) >= 0
+    let items[-1].context = {'diff': items[0:-2]}
+  endif
   return [items[-1]]
 endfunction
 
@@ -5522,6 +5524,10 @@ function! s:ToolParse(state, line) abort
     let [_, add, remove, to; __] = matchlist(a:line, '^\(\d\+\|-\)\t\(\d\+\|-\)\t\(.*\)')
     let [to, from] = s:ToolToFrom(to)
     return s:ToolItems(a:state, from, to, [], add ==# '-' ? 'Binary file' : '+' . add . ' -' . remove, add !=# '-')
+  elseif a:line =~# '^\f\+:\d\+: \D'
+    " --check
+    let [_, to, line, text; __] = matchlist(a:line, '^\(\f\+\):\(\d\+\):\s*\(.*\)$')
+    return s:ToolItems(a:state, to, to, [-1, line], text)
   elseif a:state.mode !=# 'diffhead' && a:state.mode !=# 'hunk' && len(a:line) || a:line =~# '^git: \|^usage: \|^error: \|^fatal: '
     return [{'text': a:line}]
   endif

commit c2a871af09e0e1dfe0940e9d5faa918dd569c0c5

Fix syntax error in autoload/fugitive.vim
The error was introduced in ed9e21fb9bdbda7989c1c9190ca830d36a5df4e2

autoload/fugitive.vim
@@ -5466,7 +5466,7 @@ function! s:ToolItems(state, from, to, offsets, text, ...) abort
     endif
     call add(items, item)
   endfor
-  if get(offsets, 0, 0) >= 0
+  if get(a:offsets, 0, 0) >= 0
     let items[-1].context = {'diff': items[0:-2]}
   endif
   return [items[-1]]